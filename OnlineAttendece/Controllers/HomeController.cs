9750fccf5de8ec10d57d82a72c92165fcaeda74a
Avnish Ydv <avnishydv7088@gmail.com>
2024-04-09 18:42:02 +0530

    Change Footer #

--- OnlineAttendece/Controllers/HomeController.cs
+++ OnlineAttendece/Controllers/HomeController.cs
@@ -15,9 +15,39 @@ namespace OnlineAttendece.Controllers
         Office_AttendanceEntities db = new Office_AttendanceEntities();
         public ActionResult Index()
         {
-
-            return View();
+            List<Attendence> AttreoList = new List<Attendence>();
+            // Retrieve attendance data from your data source
+            var attendanceData = db.Attendence_Master.ToList(); // Assuming Attendence_Master is the correct model
+            foreach(var  AttLoop in attendanceData)
+            {
+                AttreoList.Add(new Attendence
+                {
+                     Attendence_Id = AttLoop.Attendence_Id,
+                     Attendence_Date = (DateTime)AttLoop.Attendence_Date,
+                     Cheack_In_Time = (TimeSpan)AttLoop.Cheack_In_Time,
+                     Cheack_Out_Time = (TimeSpan)AttLoop.Cheack_In_Time
+                });
+            }
+            return View(AttreoList);
         }
+        public ActionResult Chart()
+        {
+            List<Attendence> AttreoList = new List<Attendence>();
+            // Retrieve attendance data from your data source
+            var attendanceData = db.Attendence_Master.ToList(); // Assuming Attendence_Master is the correct model
+            foreach (var AttLoop in attendanceData)
+            {
+                AttreoList.Add(new Attendence
+                {
+                    Attendence_Id = AttLoop.Attendence_Id,
+                    Attendence_Date = (DateTime)AttLoop.Attendence_Date,
+                    Cheack_In_Time = (TimeSpan)AttLoop.Cheack_In_Time,
+                    Cheack_Out_Time = (TimeSpan)AttLoop.Cheack_In_Time
+                });
+            }
+            return View(AttreoList);
+        }
+
         [HttpGet]
         public ActionResult Employee()
         {
