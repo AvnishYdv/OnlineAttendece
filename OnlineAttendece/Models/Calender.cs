c0c89b0144f9cf1db35fd9e3817fa0f7edba1b75
Avnish Ydv <avnishydv7088@gmail.com>
2024-06-14 18:21:08 +0530

    Calendar Add and Employee List Add kiya

---
+++ OnlineAttendece/Models/Calender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAttendece.Models
+{
+    public class CalendarEvent
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
